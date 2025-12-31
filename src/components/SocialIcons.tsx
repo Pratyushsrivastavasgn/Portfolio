8318a19b70f7e7ade39643fdcb84f4d2109041d8
pratyushsrivastava1sgn <pratyushsrivastava1sgn@gmail.com>
2026-01-01 00:36:54 +0530

    2026 push happy new year github and git.

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -80,7 +80,7 @@ const SocialIcons = () => {
           </a>
         </span>
       </div>
-      <a className="resume-button" href="#">
+      <a className="resume-button" href="\Pratyush's Resume 2026.pdf" target="_blank" rel="noopener noreferrer" >
         <HoverLinks text="RESUME" />
         <span>
           <TbNotes />
